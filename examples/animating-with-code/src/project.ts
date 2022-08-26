80a119597ebb4d865d419741508bb170a13d82a8
aarthificial <aarthificial@gmail.com>
2022-08-27 01:57:31 +0200

    build: update packages to 11.0.0

--- examples/animating-with-code/src/project.ts
+++ examples/animating-with-code/src/project.ts
@@ -4,12 +4,12 @@ import 'prismjs';
 import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-tsx';
 
-import declarative from './scenes/declarative.scene';
-import imperative from './scenes/imperative.scene';
-import timing from './scenes/timing.scene';
-import programming from './scenes/programming.scene';
-import explanation from './scenes/explanation.scene';
-import explanation2 from './scenes/explanation2.scene';
+import declarative from './scenes/declarative.scene?scene';
+import imperative from './scenes/imperative.scene?scene';
+import timing from './scenes/timing.scene?scene';
+import programming from './scenes/programming.scene?scene';
+import explanation from './scenes/explanation.scene?scene';
+import explanation2 from './scenes/explanation2.scene?scene';
 
 export default new Project({
   name: 'animating-with-code',
